cb357c523f04b3e8a9742ca197525ab56a296ad1
gorums <acksecurity@hotmail.com>
2017-09-17 16:32:07 -0400

    Work on the menu

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,8 +1,21 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    $(".nav a").on("click", function(){
-       $(".nav").find(".active").removeClass("active");
-       $(this).parent().parent().addClass("active");
-    });
+    if (window.location.pathname.indexOf('chart') !== -1) {
+      $(".nav li").removeClass("active");
+      $("#chart").addClass("active");
+    }
+    else if (window.location.pathname.indexOf('setting') !== -1) {
+      $(".nav li").removeClass("active");
+      $("#setting").addClass("active");
+    }
+    else if (window.location.pathname.indexOf('spaces') !== -1) {
+      $(".nav li").removeClass("active");
+      $("#spaces").addClass("active");
+    }
+    else {
+      $(".nav li").removeClass("active");
+      $("#dashboard").addClass("active");
+    }
+
 })(jQuery); // End of use strict
